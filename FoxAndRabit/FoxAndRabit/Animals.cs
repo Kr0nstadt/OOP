d5f8b3db068ccd5fec6130d847eb950537d3c0f5
Карпова Яна Алексеевна <ip316s08@csc.sibsutis.ru>
2024-07-23 23:26:37 +0700

    Первые 1,5 класса

---
+++ FoxAndRabit/FoxAndRabit/Animals.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxAndRabit
+{
+    internal abstract class Animals
+    {
+        protected int year;
+        protected int stability;
+        protected int x;
+        protected int y;
+        protected int direction;
+        protected bool life = true;
+
+        public Animals(int stability, int x, int y, int direction)
+        {
+            this.year = 0;
+            this.stability = stability;
+            this.x = x;
+            this.y = y;
+            this.direction = direction;
+        }
+
+        public abstract void Aging();
+        public abstract void Movement();
+        public abstract Animals Reproduction();
+
+        protected void Derection()
+        {
+            direction++;
+            if(direction > 3)
+            {
+                direction = 0;
+            }
+        }
+    }
+}
---
+++ FoxAndRabit/FoxAndRabit/Rabbit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxAndRabit
+{
+    internal class Rabbit : Animals
+    {
+        public Rabbit(int stability, int x, int y, int direction) : 
+            base(stability, x, y, direction) { }
+
+        public override void Aging()
+        {
+            year++;
+            if(year == 10)
+            {
+                life = false;
+            }
+        }
+        public override void Movement()
+        {
+            int NewX = 0,NewY = 0;
+            switch(direction)
+            {
+                case 0:
+                    NewX = x; NewY = y + 1; break;
+                case 1:
+                    NewX = x + 1; NewY = y; break;
+                case 2:
+                    NewX = x; NewY = y - 1; break;
+                case 3:
+                    NewX = x - 1; NewY = y; break;
+                default:
+                    break;
+            }
+            //нужно еще метод в моделе, что б оно их на другую сторону перекидывало если выход за границы
+        }
+        public override Animals Reproduction()
+        {
+            return new Rabbit(stability, x, y, direction);
+        }
+        public bool IsAlive
+        {
+            get { return life; }
+            set { life = value; }
+        }
+        public int Year//что б рожали вовремя
+        {
+            get { return year; }
+            set { year = value; }
+        }
+    }
+}
