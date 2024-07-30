bdc2996aaa9dfec2e3baf3e5dbb447bfe023f394
Карпова Яна Алексеевна <ip316s08@csc.sibsutis.ru>
2024-07-31 00:08:37 +0700

    Signed-off-by: Карпова Яна Алексеевна <ip316s08@csc.sibsutis.ru>

---
+++ FoxAndRabit/FoxAndRabitTests/CellTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FoxAndRabit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxAndRabit.Tests
+{
+    [TestClass()]
+    public class CellTests
+    {
+        [TestMethod()]
+        public void IterationTest()
+        {
+            Cell cell = new Cell();
+            Fox f1 = new Fox(2, 2, 2, 1);
+            Rabbit r1 = new Rabbit(3, 2, 1, 1);
+            Rabbit r2 = new Rabbit(4, 1, 2, 2);
+            cell.SetAnimals(r1);
+            cell.SetAnimals(r2);
+            cell.SetAnimals(f1);
+
+            int XBef = cell.list[0].X;
+            int YBef = cell.list[0].Y;
+            int CountList = cell.list.Count;
+
+            cell.Iteration();
+
+            Assert.AreNotEqual(XBef, cell.list[0].X);
+            Assert.AreNotEqual(YBef, cell.list[0].Y);
+            Assert.AreNotEqual(CountList, cell.list.Count);//зайку схавали
+        }
+    }
+}
--- FoxAndRabit/FoxAndRabitTests/FieldTests.cs
+++ FoxAndRabit/FoxAndRabitTests/FieldTests.cs
@@ -14,7 +14,22 @@ namespace FoxAndRabit.Tests
         [TestMethod()]
         public void StageFieldTest()
         {
-            Assert.Fail();
+            Fox f1 = new Fox(2, 2, 2, 1);
+            Rabbit r1 = new Rabbit(3,2,1,1);
+            Rabbit r2 = new Rabbit(4,1,2,2);
+            List<Animals> animals = new List<Animals>();
+            animals.Add(r1);
+            animals.Add(r2);
+            animals.Add(f1);
+
+            Field field = new Field(5,5,animals);
+
+            int YBef = animals[0].Y;
+            int XBef = animals[0].X;
+
+            field.StageField();
+
+            Assert.AreEqual(XBef, YBef);
         }
     }
 }
